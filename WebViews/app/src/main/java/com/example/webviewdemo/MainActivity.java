20d948d3caf2e7b21ec1f349d42db8782040c6f4
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-27 23:16:04 +0530

    Add files via upload

---
+++ WebViews/app/src/main/java/com/example/webviewdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.webviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://arcaman07.github.io/");
+        webView.loadData("<html><body><h1>Hello World!</h1><br><br><p>Always remember the night is most dark before the dawn!</p></body></html>","text/html","UTF-8");
+    }
+}
