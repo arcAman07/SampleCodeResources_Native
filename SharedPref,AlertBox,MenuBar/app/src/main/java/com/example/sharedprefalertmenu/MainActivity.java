8fa1b6237ea46f690f9b51a8c88c880a21115cc0
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-27 05:47:45 +0530

    Add files via upload

---
+++ SharedPref,AlertBox,MenuBar/app/src/main/java/com/example/sharedprefalertmenu/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.sharedprefalertmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    SharedPreferences sharedPreferences;
+    public void setLanguage(String language) {
+        sharedPreferences.edit().putString("language",language);
+        text.setText(language);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_reso,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.english:
+                setLanguage("English");
+                Log.i("Item selected","Settings");
+                return true;
+
+            case R.id.hindi:
+                setLanguage("Hindi");
+                Log.i("Item selected","Help");
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        sharedPreferences = this.getSharedPreferences("package com.example.sharedprefalertmenu", Context.MODE_PRIVATE);
+//        text = (TextView) findViewById(R.id.text);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sharedPreferences = this.getSharedPreferences("package com.example.sharedprefalertmenu", Context.MODE_PRIVATE);
+        text = (TextView) findViewById(R.id.text);
+
+        new AlertDialog.Builder(this).setTitle("Choose your Language").setMessage(
+                "Which Language would you prefer to communicate in?"
+        ).setPositiveButton("English", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                setLanguage("English");
+
+            }
+        }).setNegativeButton("Hindi", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                setLanguage("Hindi");
+
+            }
+        }).show();
+
+    }
+}
